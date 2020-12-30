0f399e6249e2d526db7396c33d7a86ee7ad3de15
Sharath (Munna) Koorathota <sharathckoo@gmail.com>
2020-12-30 17:52:25 -0500

    Update tts.py

--- src/aiy/voice/tts.py
+++ src/aiy/voice/tts.py
@@ -70,39 +70,39 @@ def google_tts_say(text, lang='en-US'):
             between 20 (slowing down by a factor of 5) and 500 (speeding up by a factor of 5).
         device: The PCM device name. Leave as ``default`` to use the default ALSA soundcard.
     """
-	# Instantiates a client
-	client = texttospeech.TextToSpeechClient()
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
 
-	# Set the text input to be synthesized
-	synthesis_input = texttospeech.SynthesisInput(text=text)
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=text)
 
-	# Build the voice request, select the language code ("en-US") and the ssml
-	# voice gender ("neutral")
-	voice = texttospeech.VoiceSelectionParams(
-	    language_code=lang, ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-	)
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code=lang, ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
 
-	# Select the type of audio file you want returned
-	audio_config = texttospeech.AudioConfig(
-	    audio_encoding=texttospeech.AudioEncoding.MP3
-	)
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
 
-	# Perform the text-to-speech request on the text input with the selected
-	# voice parameters and audio file type
-	response = client.synthesize_speech(
-	    input=synthesis_input, voice=voice, audio_config=audio_config
-	)
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
 
-	# The response's audio_content is binary.
-	with open("output.mp3", "wb") as out:
-	    # Write the response to the output file.
-	    out.write(response.audio_content)
-	    print('Audio content written to file "output.mp3"')
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
 
     pygame.init()
-	pygame.mixer.music.load('output.mp3')
-	pygame.mixer.music.play()
-	print('played')
+    pygame.mixer.music.load('output.mp3')
+    pygame.mixer.music.play()
+    print('played')
 
 
 def _main():
@@ -114,8 +114,9 @@ def _main():
     parser.add_argument('--device', default='default')
     parser.add_argument('text', help='path to disk image file ')
     args = parser.parse_args()
-    say(args.text, lang=args.lang, volume=args.volume, pitch=args.pitch, speed=args.speed,
-        device=args.device)
+    google_tts_say(args.text)
+    #say(args.text, lang=args.lang, volume=args.volume, pitch=args.pitch, speed=args.speed,
+    #    device=args.device)
 
 
 if __name__ == '__main__':
